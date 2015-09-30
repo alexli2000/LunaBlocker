de59f6f03ef278096c4f3e2a786132a23065e73b
alexli2000 <alex@Alexanders-MacBook-Pro.local>
2015-09-30 17:48:35 -0400

    InitialCommit
    Blocks ads.

--- LunaBlocker/ViewController.swift
+++ LunaBlocker/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        SFContentBlockerManager.reloadContentBlockerWithIdentifier("com.alexli.LunaBlocker.ContentBlocker") { (error) -> Void in
+            if error == nil {
+                
+            }
+        }
+    }
+    @IBAction func openSettingsApp(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string: UIApplicationOpenSettingsURLString)!)
     }
 
+    @IBAction func refreshButtonTapped(sender: AnyObject) {
+        
+        //TODO: - Allow for updated JSON blocker and refresh
+        //TODO: - Allow user to input their own blockers and refresh with this
+        
+        SFContentBlockerManager.reloadContentBlockerWithIdentifier("com.alexli.LunaBlocker.ContentBlocker") { (error) -> Void in
+            if error == nil {
+                print("Content Blocker Updated")
+            }
+        }
+    }
+    
+    @IBAction func testButtonTapped(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "http://engadget.com")!)
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
